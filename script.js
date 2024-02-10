21a489eeb62e9054b4b86bab795014b54a4e2d99
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-10 22:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ class Employee extends Person{
 jobGreet(){
 	console.log(`Hello, my name is" ${this._name}, I am `${this._age}` years old, and my job title is ${this.jobtitle}.`);
 }
+}
 
 // Do not change code below this line
 const Person = new Person("Alice", 25);
